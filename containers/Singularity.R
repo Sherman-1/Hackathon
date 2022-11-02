9240dcd532374bbfbe402dd13d47c3d4e2d924e0
Sherman-1 <herman.simon.lm@gmail.com>
2022-11-02 21:26:50 +0100

    Ca marchait ce matin ca ne marche PLUS

--- containers/Singularity.R
+++ containers/Singularity.R
@@ -1,2 +1,20 @@
 Bootstrap: docker
-From: r-base
+From: ubuntu:20.04
+
+
+%post
+	apt update -qq -y
+	
+	# Install R dependencies
+	apt install --no-install-recommends -y software-properties-common dirmngr 
+	apt install -y gnupg apt-transport-https ca-certificates 
+	
+	# Add CRAN to apt known repos
+	apt-key adv --keyserver keyserver.ubuntu.com \
+	--recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+	add-apt-repository -y 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+	
+	apt -y install r-base
+
+	# Install DESeq2 dependencies
+	apt install -y libxml2 libxml2-dev curl libssl-dev libcurl4-openssl-dev
