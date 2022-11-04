de56d3c40c3bc141fa1ab668e228fd53eb87f28b
Sherman-1 <herman.simon.lm@gmail.com>
2022-11-04 12:47:05 +0100

    DESeq2 correctement installé mais ne charge pas en librairie

--- containers/Singularity.R
+++ containers/Singularity.R
@@ -2,19 +2,37 @@ Bootstrap: docker
 From: ubuntu:20.04
 
 
-%post
-	apt update -qq -y
-	
-	# Install R dependencies
-	apt install --no-install-recommends -y software-properties-common dirmngr 
-	apt install -y gnupg apt-transport-https ca-certificates 
-	
-	# Add CRAN to apt known repos
-	apt-key adv --keyserver keyserver.ubuntu.com \
-	--recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-	add-apt-repository -y 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
-	
-	apt -y install r-base
 
-	# Install DESeq2 dependencies
-	apt install -y libxml2 libxml2-dev curl libssl-dev libcurl4-openssl-dev
+%environment 
+
+	export LC_ALL=C
+
+
+
+
+
+%post 
+
+	# Install compiling libraries and R dependencies
+	apt-get update --fix-missing -qq 
+	apt-get install -y --no-install-recommends software-properties-common dirmngr
+	apt-get install -y wget tzdata build-essential libssl-dev libz-dev libpng-dev libblas-dev \
+       			liblapack-dev libcurl4-openssl-dev libxml2-dev pkg-config gfortran
+
+	# Get latest R version as shown on CRAN website
+	wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc |  tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
+	add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/" 
+	apt-get install -y --no-install-recommends r-base 
+	
+	
+	# Install DE analysis tools
+	R -e "install.packages('BiocManager')"
+	R -e "BiocManager::install('DESeq2')" 
+
+%test 
+
+
+	R -e "1+1"
+	R -e "library('ggplot2')"
+
+
