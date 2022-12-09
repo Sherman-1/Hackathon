c237fad18656b5913b86a44596862ed7355d09eb
NoemieB <noemie.bossut1@gmail.com>
2022-12-09 15:48:09 +0100

    Check app + build 1 image

--- run.sh
+++ run.sh
@@ -1,1 +1,36 @@
-ALLEZ ZEST PARTI
+echo -e "\e[1;103;30m---------------------------------------------------------------------------------\e[0m"
+echo -e "\e[1;103;30m|   Initialisation du pipeline ATIA - BOSSUT - HERMAN                           |\e[0m"
+echo -e "\e[1;103;30m---------------------------------------------------------------------------------\e[0m"
+
+echo ""
+
+
+echo -e "\e[1;103;30m---------------------------------------------------------------------------------\e[0m"
+echo -e "\e[1;103;30m|   1 : Verification des pré-requis                                             |\e[0m"
+echo -e "\e[1;103;30m---------------------------------------------------------------------------------\e[0m"
+
+if ! command -v nextflow &> /dev/null
+then
+    echo -e "Nextflow n'a pas été trouvé, verifiez que les pré-requis sont bien installés et relancez :)"
+    exit
+fi
+
+if ! command -v singularity &> /dev/null
+then
+    echo -e "Singularity n'a pas été trouvé, verifiez que les pré-requis sont bien installés et relancez :)"
+    exit
+fi
+
+echo -e "OK"
+
+echo -e "\e[1;103;30m---------------------------------------------------------------------------------\e[0m"
+echo -e "\e[1;103;30m|   2 : Creation des images                                             |\e[0m"
+echo -e "\e[1;103;30m---------------------------------------------------------------------------------\e[0m"
+
+cd containers
+
+cd featurecounts
+echo -e "FeatureCounts:"
+singularity build featcount.sif Singularity.sratoolkit --fakeroot
+
+echo "Vive les DOGGOS"
